b5d80c31775dd9a769dfa5a440023c9783de6f64
The S Orbital <95533939+S-Orbital@users.noreply.github.com>
2025-01-30 11:26:09 -0500

    Testing function exports

---
+++ alphastats.js
@@ -0,0 +1,3 @@
+export function consoleTest() {
+  console.error("You are not sigma enough to obtain the true content of this error message, so this temporary error message was placed. To become more sigma, you must improve your physique by enhancing your jawline until it reaches a status of chiseled, and then try again.");
+}
